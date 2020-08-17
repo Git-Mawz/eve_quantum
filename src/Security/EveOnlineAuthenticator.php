d7c1912385bba1f7e0e7cc33a8a17a6e50d685f4
Git-Mawz <py.oclock@gmail.com>
2020-08-17 16:00:06 +0200

    added EveOauth2 service in order to clarify EveOnlineAuthenticator

--- src/Security/EveOnlineAuthenticator.php
+++ src/Security/EveOnlineAuthenticator.php
@@ -2,6 +2,7 @@
 
 namespace App\Security;
 
+use App\Service\EveOauth2;
 use App\Service\UserChecker;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -19,15 +20,17 @@ class EveOnlineAuthenticator extends AbstractGuardAuthenticator
     public const LOGIN_ROUTE = 'app_login';
 
     private $userChecker;
+    private $eveOauth2;
     private $session;
-    private $resourceOwner;
-    private $accessToken;
+    // private $resourceOwner;
+    // private $accessToken;
 
 
-    public function __construct(UserChecker $userChecker, SessionInterface $session)
+    public function __construct(UserChecker $userChecker, SessionInterface $session, EveOauth2 $eveOauth2)
     {
         $this->session = $session;
         $this->userChecker = $userChecker;
+        $this->eveOauth2 = $eveOauth2;
       
     }
 
@@ -45,52 +48,7 @@ class EveOnlineAuthenticator extends AbstractGuardAuthenticator
 
     public function getCredentials(Request $request)
     {
-        $provider = new \Killmails\OAuth2\Client\Provider\EveOnline([
-            'clientId'          => $_ENV["CLIENT_ID"],
-            'clientSecret'      => $_ENV["SECRET_KEY"],
-            'redirectUri'       => 'http://localhost:8000/login',
-        ]);
-        
-        // If we don't have an authorization code then get one
-        if (!isset($_GET['code'])) {
-        
-            // Fetch the authorization URL from the provider; this returns the
-            // urlAuthorize option and generates and applies any necessary parameters
-            // (e.g. state).
-            $options = [
-                'scope' => ['esi-mail.read_mail.v1', 'esi-mail.send_mail.v1'] // array or string
-            ];
-            
-            $authorizationUrl = $provider->getAuthorizationUrl($options);
-        
-            // Redirect the user to the authorization URL.
-            header('Location: ' . $authorizationUrl);
-            exit;
-        
-        } else {
-        
-            try {
-        
-                // Try to get an access token using the authorization code grant.
-                $accessToken = $provider->getAccessToken('authorization_code', [
-                    'code' => $_GET['code']
-                ]);
-        
-                $resourceOwner = $provider->getResourceOwner($accessToken);
-                
-                $this->resourceOwner = $resourceOwner;
-                $this->accessToken = $accessToken;
-
-            } catch (\League\OAuth2\Client\Provider\Exception\IdentityProviderException $e) {
-        
-                // Failed to get the access token or user details.
-                exit($e->getMessage());
-        
-            }
-
-            return $resourceOwner;
-
-        }
+        return $this->eveOauth2->authenticate();
     }
 
     public function getUser($credentials, UserProviderInterface $userProvider)
@@ -120,8 +78,8 @@ class EveOnlineAuthenticator extends AbstractGuardAuthenticator
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
     {
-        $this->session->set('resourceOwner', $this->resourceOwner);
-        $this->session->set('accessToken', $this->accessToken);
+        $this->session->set('resourceOwner', $this->eveOauth2->getResourceOwner());
+        $this->session->set('accessToken', $this->eveOauth2->getAccessToken());
     }
 
     public function start(Request $request, AuthenticationException $authException = null)
---
+++ src/Service/EveOauth2.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\HttpFoundation\Request;
+
+class EveOauth2
+{
+    public $resourceOwner;
+    public $accessToken;
+    
+    public function getResourceOwner()
+    {
+        return $this->resourceOwner;
+    }
+
+    public function getAccessToken()
+    {
+        return $this->accessToken;
+    }
+
+    public function authenticate()
+    {
+
+    $provider = new \Killmails\OAuth2\Client\Provider\EveOnline([
+        'clientId'          => $_ENV["CLIENT_ID"],
+        'clientSecret'      => $_ENV["SECRET_KEY"],
+        'redirectUri'       => 'http://localhost:8000/login',
+        ]);
+        
+    // If we don't have an authorization code then get one
+    if (!isset($_GET['code'])) {
+        
+        // Fetch the authorization URL from the provider; this returns the
+        // urlAuthorize option and generates and applies any necessary parameters
+        // (e.g. state).
+        $options = [
+            'scope' => ['esi-mail.read_mail.v1', 'esi-mail.send_mail.v1'] // array or string
+        ];
+        
+        $authorizationUrl = $provider->getAuthorizationUrl($options);
+        
+        // Redirect the user to the authorization URL.
+        header('Location: ' . $authorizationUrl);
+        exit;
+        
+    } else {
+        
+        try {
+            
+            // Try to get an access token using the authorization code grant.
+            $accessToken = $provider->getAccessToken('authorization_code', [
+                'code' => $_GET['code']
+                ]);
+                
+                $resourceOwner = $provider->getResourceOwner($accessToken);
+                
+                $this->resourceOwner = $resourceOwner;
+                $this->accessToken = $accessToken;
+                
+            } catch (\League\OAuth2\Client\Provider\Exception\IdentityProviderException $e) {
+                
+                // Failed to get the access token or user details.
+                exit($e->getMessage());
+                
+            }    
+            return $resourceOwner;          
+        }
+    }
+}
