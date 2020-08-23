8034139dd50ec10c640269e4aee5872a0ee43893
Git-Mawz <py.oclock@gmail.com>
2020-08-23 22:53:44 +0200

    added event suscriber to refresh token if he expired

---
+++ src/EventSubscriber/EveTokenRefreshSubscriber.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+
+class EveTokenRefreshSubscriber implements EventSubscriberInterface
+{   
+    private $session;
+
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
+
+    public function onKernelRequest(RequestEvent $event)
+    {   
+        $provider = new \Killmails\OAuth2\Client\Provider\EveOnline([
+            'clientId'          => $_ENV["CLIENT_ID"],
+            'clientSecret'      => $_ENV["SECRET_KEY"],
+            'redirectUri'       => 'http://localhost:8000/login',
+            ]);
+
+        $existingAccessToken = $this->session->get('accessToken');
+        
+        if ($existingAccessToken != null && $existingAccessToken->hasExpired()) {
+            $newAccessToken = $provider->getAccessToken('refresh_token', [
+                'refresh_token' => $existingAccessToken->getRefreshToken()
+                ]);
+                
+            $this->session->set('accessToken', $newAccessToken);
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'kernel.request' => 'onKernelRequest',
+        ];
+    }
+}
