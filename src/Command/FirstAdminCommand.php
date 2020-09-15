5e371ce92dfed303e744701e0e83f9e7ed3a0fec
Git-Mawz <py.oclock@gmail.com>
2020-09-15 12:39:16 +0200

    modified command to create first admin on both public and admin dashboard

--- src/Command/FirstAdminCommand.php
+++ src/Command/FirstAdminCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Command;
 
+use App\Entity\Admin;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
@@ -31,18 +32,31 @@ class FirstAdminCommand extends Command
     {
         $this
             ->setDescription('Allow to set a first admin to the Eve Quantum application')
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+            ->addArgument('arg1', InputArgument::REQUIRED, 'Admin username')
+            ->addArgument('arg2', InputArgument::REQUIRED, 'Admin password')
+            // ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
-        
+        $adminUsername = $input->getArgument('arg1');
+        $adminPassword = $input->getArgument('arg2');
+
+        // Adding admin role on Krawks for the public site firewall
         $firstAdmin = $this->userRepository->findOneBy(['character_owner_hash' => static::FIRST_ADMIN_CHARACTER_OWNER_HASH]);
         $firstAdmin->setRoles(['ROLE_ADMIN']);
+
+        // Creating the first admin for the dashboard/easyadmin firewall
+        $newDashboardAdmin = new Admin();
+        $newDashboardAdmin->setUsername($adminUsername);
+        $newDashboardAdmin->setPassword($adminPassword);
+        $newDashboardAdmin->setRoles(['ROLE_ADMIN']);
+        $this->em->persist($newDashboardAdmin);
+
+
+        // Final flush
         $this->em->flush();
 
 
