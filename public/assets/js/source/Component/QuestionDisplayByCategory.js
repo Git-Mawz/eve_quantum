0f81c16f041a3bc56d79241b7d9b9d918ae5d244
Git-Mawz <py.oclock@gmail.com>
2020-10-12 19:22:52 +0200

    using NaN on dataset category

--- public/assets/js/source/Component/QuestionDisplayByCategory.js
+++ public/assets/js/source/Component/QuestionDisplayByCategory.js
@@ -4,53 +4,57 @@ class QuestionDisplayByCategory
   _curentTarget;
   _categoryId;
   _tbodyInitialInnerHTML;
+  _searchNavItems;
 
   constructor() {
     this._tbodyInitialInnerHTML = document.querySelector('.search-result-tbody').innerHTML;
+    this._searchNavItems = document.querySelector('.nav-search')
   }
 
 
   initialize() {
     console.log('initialize QuestionDisplayByCategory');
-
-    const manager = new QuestionManager();
+    console.log(this._searchNavItems);
     document.addEventListener('click', (event) => {this.handleClickOnCategory(event)})
-
   }
 
   handleClickOnCategory(event) {
-      console.log(event.target);
+      // console.log(event.target);
       this._curentTarget = event.target;
       this._categoryId = this._curentTarget.dataset.category;
 
-      console.log(this._categoryId);
+      // console.log(this._categoryId);
+      if (isNaN(this._categoryId)) {
+        console.log('Dataset category NaN')
+      } else {
 
-      // Initialize baseUrl
-      let baseUrl = new BaseUrl();
-      baseUrl = baseUrl.getBaseUrl();
-
-      // initialize tbody
-      let tbody = document.querySelector('.search-result-tbody');
-
-      const manager = new QuestionManager();
-      
-      manager.loadQuestionByCategory(this._categoryId).then((questionList) => {
+        // Initialize baseUrl
+        let baseUrl = new BaseUrl();
+        baseUrl = baseUrl.getBaseUrl();
         
-        // init of tbody to prepare search results
-        tbody.innerHTML = '';
+        // initialize tbody
+        let tbody = document.querySelector('.search-result-tbody');
         
-        for (let question of questionList) {
+        const manager = new QuestionManager();
+        
+        manager.loadQuestionByCategory(this._categoryId).then((questionList) => {
           
-          let date = new Date(question.getCreatedAt());
+          // init of tbody to prepare search results
+          tbody.innerHTML = '';
           
-          tbody.innerHTML += 
-          '<td><a href="' + baseUrl + '/read/' + question.getSlug() + '">' + question.getTitle() + '</a></td>' +
-          '<td>' + question.getCategory().name + '</td>' +
-          '<td scope="row">' + question.getUser().name + '</td>' +
-          '<td>' + date.getDate()+ '/' + date.getMonth() + '/' + date.getFullYear() + ' à ' + date.getHours() + ':' + date.getMinutes() + '</td>';
-          
-        }
-      });
+          for (let question of questionList) {
+            
+            let date = new Date(question.getCreatedAt());
+            
+            tbody.innerHTML += 
+            '<td><a href="' + baseUrl + '/read/' + question.getSlug() + '">' + question.getTitle() + '</a></td>' +
+            '<td>' + question.getCategory().name + '</td>' +
+            '<td scope="row">' + question.getUser().name + '</td>' +
+            '<td>' + date.getDate()+ '/' + date.getMonth() + '/' + date.getFullYear() + ' à ' + date.getHours() + ':' + date.getMinutes() + '</td>';
+            
+          }
+        });
+      }
 
   }
 
