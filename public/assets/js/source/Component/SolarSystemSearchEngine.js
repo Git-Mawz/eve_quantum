8f4f507cbe79f476b93fb58b96416cdd81281eb2
Git-Mawz <py.oclock@gmail.com>
2020-10-19 15:36:40 +0200

    create API for Character (/profile) processing with AJAX, and system search OK

--- public/assets/js/source/Component/SolarSystemSearchEngine.js
+++ public/assets/js/source/Component/SolarSystemSearchEngine.js
@@ -6,12 +6,14 @@ class SolarSystemSearchEngine
     _form;
     _input;
     _manager;
+    _ul;
 
     constructor(baseUrl, form, input, manager) {
         this._baseUrl = baseUrl;
         this._form = form;
         this._input = input;
         this._manager = manager;
+        this._ul = document.querySelector('.solar-system-list');
       }
 
     initialize() {
@@ -27,21 +29,33 @@ class SolarSystemSearchEngine
 
         this._manager.searchSolarSystem(this._input.value).then((promiseList) => {
             
+            this._ul.innerHTML = '';
+
             return Promise.all(promiseList).then((responses) => {
-                console.log(responses);
-                const solarSytems = responses[0];
-                //.....
-                console.log(responses);
-                // console.log(solarSytems);
+                const solarSytems = responses;
+                // console.log(solarSytems)
+                for (let solarSytem of solarSytems) {
+                    // console.log(solarSytem.getName());
+                    this.makeSolarSystemList(solarSytem);
+                } 
 
-                return solarSytems;
-            });
+            })
 
-        });
-
-
-        
+        }); 
     }
+
+    makeSolarSystemList(solarSytem) {
+
+        console.log(solarSytem);
+
+        let li = document.createElement('li');
+        let textNode = document.createTextNode(solarSytem.getName());
+        li.appendChild(textNode);
+
+        this._ul.appendChild(li);
+
+    }
+
 
 
 }
---
+++ src/Controller/Api/CharacterController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Entity\SolarSystem;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+/**
+ * @Route("/api/character", name="api_character_")
+ */
+class CharacterController extends AbstractController
+{
+    /**
+     * @Route("/solar_system", name="solar_system_add", methods={"POST"})
+     */
+    public function addFavoriteSystem(Request $request, EntityManagerInterface $em)
+    {   
+        $this->denyAccessUnlessGranted("ROLE_USER");
+
+        $jsonData = json_decode($request->getContent(), true);
+
+        $newSolarSystem = new SolarSystem();
+        $newSolarSystem->setName($jsonData['name']);
+        $newSolarSystem->setUniverseId($jsonData['universeId']);
+
+        
+        $em->persist($newSolarSystem);
+        
+        $currentUser = $this->getUser();
+        $currentUser->addSolarSystem($newSolarSystem);
+
+        $em->flush();
+
+
+        return $this->json([
+            'message' => $newSolarSystem->getName() . ' added to ' . $currentUser->getName() . '\'s favorite solar system',
+        ]);
+    }
+
+    /**
+     * @Route("/solar_system/{universeId}", name="solar_system_delete", methods={"DELETE"})
+     */
+    public function removeFavoriteSystem(SolarSystem $solarSystem, Request $request, EntityManagerInterface $em)
+    {
+        // $this->denyAccessUnlessGranted("ROLE_USER");
+
+        
+
+        $currentUser = $this->getUser();
+        $currentUser->removeSolarSystem($solarSystem);
+
+        $em->flush();
+
+        return $this->json([
+            'message' => $solarSystem->getName() . ' removed from ' . $currentUser->getName() . '\'s favorite solar system',
+        ]);
+
+    }
+
+}
