d0ea57ed66520d518e8a78498b1310f27247efdf
Git-Mawz <py.oclock@gmail.com>
2020-10-21 19:21:33 +0200

    added api route to browse user favorite solar systems

--- src/Controller/Api/CharacterController.php
+++ src/Controller/Api/CharacterController.php
@@ -8,7 +8,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\Serializer\SerializerInterface;
 
 /**
  * @Route("/api/character", name="api_character_")
@@ -41,7 +41,7 @@ class CharacterController extends AbstractController
      */
     public function removeFavoriteSystem(SolarSystem $solarSystem, Request $request, EntityManagerInterface $em)
     {
-        // $this->denyAccessUnlessGranted("ROLE_USER");
+        $this->denyAccessUnlessGranted("ROLE_USER");
 
         $currentUser = $this->getUser();
         $currentUser->removeSolarSystem($solarSystem);
@@ -53,5 +53,20 @@ class CharacterController extends AbstractController
         ]);
 
     }
+
+    /**
+     * @Route("/solar_system/", name="solar_system_browse", methods={"GET"})
+     */
+    public function browseFavoriteSystems (SerializerInterface $serializer)
+    {
+        $this->denyAccessUnlessGranted("ROLE_USER");
+
+        $userFavoriteSolarSystems = $this->getUser()->getSolarSystem();
+        $data = $serializer->normalize($userFavoriteSolarSystems, 'json', ['groups' => ['favorite_solar_system']]);
+
+        return $this->json(['solarSystems' => $data]);
+
+    }
+
 
 }
