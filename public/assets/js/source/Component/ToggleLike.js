a192fd8fafdc0c315800f6ae5f0f5a85d794d46b
Git-Mawz <py.oclock@gmail.com>
2020-10-31 15:53:00 +0100

    fonctionnal like feature

--- public/assets/js/source/Component/ToggleLike.js
+++ public/assets/js/source/Component/ToggleLike.js
@@ -17,14 +17,45 @@ class ToggleLike
 
     handleLikeFormSubmit(event) {
         event.preventDefault();
-        const answerId = event.target.dataset.answerId;
+        const eventTarget = event.target;
+        const answerId = eventTarget.dataset.answerId;
+
+        const checkIcon = eventTarget.querySelector('.check-icon');
+        const likeCounter = document.querySelector('.like-counter-' + answerId)
+
         this._likeRepository.toggleLike(answerId).then((response) => {
             // console.log(response);
             const likeStatus = response['likeStatus'];
-            console.log(likeStatus);
-            
+            // console.log(likeStatus);
+            if(likeStatus) {
+                // console.log(likeStatus);
+                this.addLikeDisplay(checkIcon, likeCounter)
+            } else {
+                // console.log(likeStatus);
+                this.removeLikeDisplay(checkIcon, likeCounter)
+            }
 
         })
+    }
+
+    addLikeDisplay(checkIcon, likeCounter) {
+        // dynamically change counter value
+        let LikeCounterValue = parseInt(likeCounter.innerHTML, 10);
+        let newLikeCounterValue = LikeCounterValue+1;
+        likeCounter.innerHTML = newLikeCounterValue;
+        // dynamically change checkIcon color
+        checkIcon.style.color = 'green';
+        
+    }
+
+    removeLikeDisplay(checkIcon, likeCounter) {
+        // dynamically change counter value
+        let LikeCounterValue = parseInt(likeCounter.innerHTML, 10);
+        let newLikeCounterValue = LikeCounterValue-1;
+        likeCounter.innerHTML = newLikeCounterValue;
+        // dynamically change checkIcon color
+        checkIcon.style.color = '#AAAAAA';
+
     }
 
 }
