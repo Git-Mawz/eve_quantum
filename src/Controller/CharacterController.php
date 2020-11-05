d37da70b0f142399d4f0b23d91f3f8f374d57658
Git-Mawz <py.oclock@gmail.com>
2020-11-05 12:29:29 +0100

    working other user profiles

--- src/Controller/CharacterController.php
+++ src/Controller/CharacterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\AnswerRepository;
 use App\Repository\QuestionRepository;
 use App\Repository\UserRepository;
@@ -17,7 +18,7 @@ class CharacterController extends AbstractController
     {
         $user = $this->getUser();
         
-        // on cherche a afficher la dernière question ou la dernière réponse de l'utilisateur en affichant la dernière en date
+        // TODO make a custom query to get last question or last answer for a given user
         $lastQuestion = $questionRepository->findUserLastQuestion($user);
         $lastAnswer = $answerRepository->findUserLastAnswer($user);
 
@@ -34,27 +35,53 @@ class CharacterController extends AbstractController
         } else {
             $lastSubject = null;
         }
-            
         
-        // $userAnswers = $user->getAnswers();
-        // $receivedLikes = 0;
-        // foreach($userAnswers as $userAnswer) {
-        //     $receivedLikes += count($userAnswer->getLikes());
-        // }
-            
-        $receivedLikes = $userRepository->findAllReceivedLikes($user);
-        // dd($receivedLikes);
+        $userReceivedLikes = $userRepository->findAllReceivedLikes($user);
 
         return $this->render('character/profile.html.twig', [
             'userQuestionsCount' => count($user->getQuestions()),
             'userAnswersCount' => count($user->getAnswers()),
             'lastSubject' => $lastSubject,
-            'userTotalLikes' => $receivedLikes
+            'userReceivedLikes' => $userReceivedLikes,
+            'userGivenLikes' => count($user->getLikes())
         ]);
     }
 
 
-    
+    /**
+     * @Route("/profile/{character_owner_hash}", name="other_character_profile", requirements={"character_owner_hash"=".+"})
+     */
 
+    public function otherProfile (User $foreignUser, QuestionRepository $questionRepository, AnswerRepository $answerRepository, UserRepository $userRepository)
+    {
 
+        $lastQuestion = $questionRepository->findUserLastQuestion($foreignUser);
+        $lastAnswer = $answerRepository->findUserLastAnswer($foreignUser);
+
+        if ($lastQuestion && $lastAnswer) {
+            if($lastQuestion->getCreatedAt() > $lastAnswer->getCreatedAt()) {
+                $lastSubject = $lastQuestion;
+            } else {
+                $lastSubject = $lastAnswer->getQuestion();
+            }
+        } else if ($lastQuestion && !$lastAnswer) {
+            $lastSubject = $lastQuestion;
+        } else if (!$lastQuestion && $lastAnswer) {
+            $lastSubject = $lastAnswer->getQuestion();
+        } else {
+            $lastSubject = null;
+        }
+
+        $userReceivedLikes = $userRepository->findAllReceivedLikes($foreignUser);
+
+        
+        return $this->render('character/other_profile.html.twig', [
+            'foreignUser' => $foreignUser,
+            'userQuestionsCount' => count($foreignUser->getQuestions()),
+            'userAnswersCount' => count($foreignUser->getAnswers()),
+            'lastSubject' => $lastSubject,
+            'userReceivedLikes' => $userReceivedLikes,
+            'userGivenLikes' => count($foreignUser->getLikes())
+        ]);
+    }
 }
