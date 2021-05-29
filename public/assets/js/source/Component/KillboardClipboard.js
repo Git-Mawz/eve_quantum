47d694875d6fc7a5935607d1057d1d02a4a70526
Git-Mawz <py.oclock@gmail.com>
2021-05-29 23:21:55 +0200

    added fetching shipsData utils

--- public/assets/js/source/Component/KillboardClipboard.js
+++ public/assets/js/source/Component/KillboardClipboard.js
@@ -1,62 +1,30 @@
 class KillboardClipboard {
 
-
     _zkillButton;
     _analyzeButton;
+    _shipsData;
 
     constructor() {
-        // console.log('KillboardClipboard loaded');
-    }
-
-    initialize() {
-        // console.log('KillboardClipboard initialized');
         this._zkillButton = document.getElementById('zkill');
         this._analyzeButton = document.getElementById('analyze');
+    }
 
-
-
+    async initialize() {
         this._zkillButton.addEventListener('click', (event) => {
             this.openZkillboardTab();
         })
-
         this._analyzeButton.addEventListener('click', (event) => {
             this.getZkillCharacterStats();
         });
 
-
-        this.getZkillCharacterStats(this._analyzeButton);
+        // On charge la liste des ships si elle n'est pas déjà présente dans le session storage
+        if (!sessionStorage.getItem('shipsData')) {
+            this._shipsData = await this.getAllShipInfo();
+            console.log('shipData loaded in session storage');
+        }
     }
 
-    async getZkillCharacterStats() {
-
-        // refresh toutss
-        let truc = await this.getCharacterIdFromESI();
-        console.log(truc);
-
-
-    }
-
-    async getCharacterIdFromESI() {
-        let text = await navigator.clipboard.readText()
-
-        let names = JSON.stringify([text]);
-
-        return await fetch("https://esi.evetech.net/latest/universe/ids/?datasource=tranquility&language=en",
-        {
-            method: "POST",
-            body: names,
-            headers: {
-                'Content-Type': 'application/json'
-            },
-
-        }).then(res => { console.log(res); return res.json(); })
-        .then(data => {
-            return data.characters[0].id;
-        });
-    }
-
-    openZkillboardTab(element) {
-        //element.addEventListener('click', (event) => {
+    openZkillboardTab() {
         navigator.clipboard.readText()
         .then(text => {
             let names = JSON.stringify([text]);
@@ -75,21 +43,90 @@ class KillboardClipboard {
                 window.open(zkillURL, '_blank');
             })
         });
-        //})
     }
 
-    // getZkillCharacterStats(element) {
-    //     element.addEventListener('click', (event) => {
-    //         fetch('https://zkillboard.com/api/stats/characterID/' + 1294095600 +'/')
-    //             .then(function(res){ return res.json(); })
-    //             .then(function (data) {
-    //             console.log(data);
-    //         }) 
-    //     })
-    // }
 
+    // Utils
+    async getZkillCharacterStats() {
+        let character = await this.getCharacterFromESI();
+        return await fetch('https://zkillboard.com/api/stats/characterID/' + character.id +'/')
+            .then(function(res){ return res.json(); })
+            .then(function (characterStats) {
+                console.log(characterStats);
+                return characterStats;
+            }) 
+    }
 
+    async getCharacterFromESI() {
+        let clipboardContent = await navigator.clipboard.readText()
+        let names = JSON.stringify([clipboardContent]);
+        return await fetch("https://esi.evetech.net/latest/universe/ids/?datasource=tranquility&language=en",
+        {
+            method: "POST",
+            body: names,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        }).then(res => { return res.json(); })
+        .then(data => {
+            return data.characters[0];
+        });
+    }
 
+    async getAllianceName(allianceID) {
+        return await fetch('https://esi.evetech.net/latest/alliances/' + allianceID + '/?datasource=tranquility')
+            .then(function(res){ return res.json(); })
+            .then(function (alliance) {
+                console.log(alliance);
+                return alliance.name;
+            }) 
+    }
+
+    async getCorpName(corpID) {
+        return await fetch('https://esi.evetech.net/latest/corporations/' + corpID + '/?datasource=tranquility')
+            .then(function(res){ return res.json(); })
+            .then(function (corp) {
+                console.log(corp);
+                return corp.name;
+            }) 
+    }
+
+    /**
+     * On récupère les noms de tous les ships et leurs ids
+     * Si ils ont déjà été appeler on les récupère directement dans le session storage
+     */
+    async getAllShipInfo () {
+        if (sessionStorage.getItem('shipsData')) {
+            console.log('shipsData from session storage');
+            return JSON.parse(sessionStorage.getItem('shipsData'));
+        }
+
+        return await fetch('https://esi.evetech.net/v1/insurance/prices/?datasource=tranquility&language=en')
+        .then(function(res){ return res.json(); })
+        .then(function (data) {
+            let allShipID = []
+            for (let shipData of data) {
+                allShipID.push(shipData.type_id);
+            }
+            return allShipID;
+        })
+        // on va récup tout les noms des ships à partir des ID
+        .then(shipIdArray => {
+            let shipIds = JSON.stringify(shipIdArray);
+            return fetch("https://esi.evetech.net/v2/universe/names/?datasource=tranquility&language=en",
+            {
+                method: "POST",
+                body: shipIds,
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            }).then(res => { return res.json(); })
+            .then(data => {
+                sessionStorage.setItem('shipsData', JSON.stringify(data));
+                return data;
+            });
+        })
+    }
 
 
 }
